a5ebe5c7713932abf4797c4c40c081406be33d54
mkrupczak3 <matthew@krupczak.org>
2023-03-16 16:35:54 -0400

    Create FeedMe.sh

---
+++ FeedMe.sh
@@ -0,0 +1,2 @@
+#!/usr/env/bin/bash
+find . -type f -not -path '*/\.*' -exec grep -Il '.' {} \; | xargs awk 'FNR==1{print "--------------------------------------------------------------------------------"; print FILENAME; print "--------------------------------------------------------------------------------"; print ""; }{ print; print "" }' > allcode.txt
