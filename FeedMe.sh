363c673d76390ca470d24108344658fa017bdc28
bob joe <matthew@krupczak.org>
2023-03-16 16:46:52 -0400

    FeedMe.sh fix bash shebang

--- FeedMe.sh
+++ FeedMe.sh
@@ -1,2 +1,2 @@
-#!/usr/env/bin/bash
+#!/usr/bin/env bash
 find . -type f -not -path '*/\.*' -exec grep -Il '.' {} \; | xargs awk 'FNR==1{print "--------------------------------------------------------------------------------"; print FILENAME; print "--------------------------------------------------------------------------------"; print ""; }{ print; print "" }' > allcode.txt
